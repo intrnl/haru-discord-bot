8cbb11f96969769e8c9e18be1ee8e775921f9210
intrnl <intrnl28+git@gmail.com>
2020-12-12 00:56:31 +0700

    use iso date string

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,5 +2,5 @@ import { define } from '../lib/handler';
 
 
 define('ping', ({ member }) => {
-	return `Hello, ${member.nick ?? member.user.username}! It is ${new Date().toUTCString()} currently.`;
+	return `Hello, ${member.nick ?? member.user.username}! It is ${new Date().toISOString()} currently.`;
 });
